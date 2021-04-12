5dc2cb52064d8a6d6e17a1670e72db3636fe5ac1
Pawel Sydorowicz <pawelsydorowicz@gmail.com>
2021-04-12 23:08:17 +0200

    jama comit

--- application/routes/web.php
+++ application/routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\documentsController::class, 'index']);
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'document']);
