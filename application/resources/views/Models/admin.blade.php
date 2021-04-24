c0489c462c7acaa81d39add7b1cf575fe43b723e
Pawel Sydorowicz <pawelsydorowicz@gmail.com>
2021-04-24 21:35:32 +0200

    dodanie klawiszy edycji i usunięcia z pozycji admin

--- application/resources/views/Models/admin.blade.php
+++ application/resources/views/Models/admin.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+  <div class="d-grid gap-2">
+    <a href="a_soldiers"><button class="button btn-primary" type="button">Soldiers</button></a>
+    <a href=""><button class="button btn-primary btn-sm">Tanks</button></a>
+    <a href=""><button class="button btn-primary btn-sm">Documents</button></a>
+    {{-- <a href="/addSoldier"><button>Add a Soldier</button></a> --}}
+    {{-- <a href="/addTank"><button>Add a Tank</button></a> --}}
+  </div>
+</div>
 @endsection
--- application/resources/views/Models/editSoldier.blade.php
+++ application/resources/views/Models/editSoldier.blade.php
@@ -2,25 +2,52 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    <form method="POST" action="/updateSoldier/{id}">
-                        @csrf
-<!-- Type pass number -->
-                        <div class="form-group row">
-                            <label for="pass_number" class="col-md-4 col-form-label text-md-right">{{ __('Pass number') }}</label>
-                            <div class="col-md-6">
-                                <input id="pass_number" type="text" class="form-control @error('pass_number') is-invalid @enderror" name="pass_number" value="{{ $user->pass_number }}" required autocomplete="pass_number" autofocus>
-                                @error('pass_number')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-<!-- Type name -->
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<form method="POST" action="/updateSoldier/{id}">
+						@csrf
+						<!-- Type pass number -->
+						<div class="form-group row">
+							<label for="pass_number" class="col-md-4 col-form-label text-md-right">{{ __('Pass number') }}</label>
+							<div class="col-md-6">
+								<input id="pass_number" type="text" class="form-control @error('pass_number') is-invalid @enderror" name="pass_number" value="{{ $user->pass_number }}" required autocomplete="pass_number" autofocus>
+								@error('pass_number')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+								@enderror
+							</div>
+						</div>
+						<!-- Type rank -->
+						<div class="form-group row">
+							<label for="rank" class="col-md-4 col-form-label text-md-right">{{ __('Rank') }}</label>
+							<div class="col-md-6">
+								<select id="rank" type="text" class="form-control" @error('rank') is-invalid @enderror" name="rank" required autocomplete="rank" autofocus>
+									<option>-- wybierz z listy --</option>
+									<option>szer.</option>
+									<option>st. szer.</option>
+									<option>kpr.</option>
+									<option>st. kpr.</option>
+									<option>plut.</option>
+									<option>st. plut.</option>
+									<option>sierż.</option>
+									<option>st. sierż.</option>
+									<option>mł. chor.</option>
+									<option>chor.</option>
+									<option>st. chor.</option>
+									<option>st. chor. sztab.</option>
+									<option>ppor.</option>
+									<option>por.</option>
+									<option>kpt.</option>
+									<option>mjr.</option>
+									<option>ppłk.</option>
+									<option>płk.</option>
+								</select>
+							</div>
+						</div>
+						<!-- Type name -->
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                             <div class="col-md-6">
@@ -44,30 +71,47 @@
                                 @enderror
                             </div>
                         </div>
-<!-- Type rank -->
-                        <div class="form-group row">
-                            <label for="rank" class="col-md-4 col-form-label text-md-right">{{ __('Rank') }}</label>
-                            <div class="col-md-6">
-                                <input id="rank" type="text" class="form-control @error('rank') is-invalid @enderror" name="rank" value="{{ $user->rank }}" required autocomplete="rank" autofocus>
-                                @error('rank')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+												<!-- Type function -->
+												<div class="form-group row">
+													<label for="function" class="col-md-4 col-form-label text-md-right">{{ __('Function') }}</label>
+													<div class="col-md-6">
+														<select id="function" type="text" class="form-control" @error('function') is-invalid @enderror" name="function" required autocomplete="function" autofocus>
+															<option>-- wybierz z listy --</option>
+															<option>dowódca kompanii</option>
+															<option>szef kompanii</option>
+															<option>technik kompanii</option>
+															<option>technik uzbrojenia</option>
+															<option>dowódca plutonu</option>
+															<option>pomocnik dowódcy plutonu</option>
+															<option>kierowca - starszy instruktor</option>
+															<option>kierowca</option>
+														</select>
+					
+														@error('function')
+														<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+														@enderror
+					
+													</div>
+												</div>
 <!-- Type platoon -->
-                        <div class="form-group row">
-                            <label for="platoon" class="col-md-4 col-form-label text-md-right">{{ __('Platoon') }}</label>
-                            <div class="col-md-6">
-                                <input id="platoon" type="text" class="form-control @error('platoon') is-invalid @enderror" name="platoon" value="{{ $user->platoon }}" required autocomplete="platoon" autofocus>
-                                @error('platoon')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<div class="form-group row">
+	<label for="platoon" class="col-md-4 col-form-label text-md-right">{{ __('Platoon') }}</label>
+	<div class="col-md-6">
+		<select id="platoon" type="text" class="form-control" @error('platoon') is-invalid @enderror" name="platoon" value="{{ old('platoon') }}" required autocomplete="platoon" autofocus>
+			<option>-- wybierz z listy --</option>
+			<option>nie dotyczy</option>
+			<option>I</option>
+			<option>II</option>
+			<option>III</option>
+			<option>IV</option>
+		</select>
+
+		@error('platoon')
+		<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+		@enderror
+
+	</div>
+</div>
 <!-- Type e-mail -->
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
